20b454b0df36af56a4ecece734335fb204402eb7
MarcosInacio <marcos.inacio.recife@hotmail.com>
2015-05-08 16:33:38 -0300

    projeto

---
+++ src/NovoProjeto/src/br/ufrpe/sistema_de_entrega_de_comida/Principal.java
@@ -0,0 +1,13 @@
+package br.ufrpe.sistema_de_entrega_de_comida;
+
+import br.ufrpe.sistema_de_entrega_de_comida_dados.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+			RepositorioNomes repositorio = new RepositorioNomes();
+			repositorio.repositorioNomes();
+
+	}
+}
